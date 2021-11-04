2bbc659d05f5a7b205c18588d220881b1279ddcb
GonzaloB97 <gonzalobarlaro97@gmail.com>
2021-11-04 00:36:18 -0300

    intentando json y js

---
+++ TP/js/merch.js
@@ -0,0 +1,8 @@
+console.log(Hola mundo!);
+
+const url = 'merch.json';
+const Request = new XMLHttpRequest();
+request.open('GET',url);
+request.responseType = 'json';
+request.send();
+var articulo
