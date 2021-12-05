7dbc500d802b6a11a257f0ed79548d8b870ba593
AmiraK14 <amirakollrich@gmail.com>
2021-12-05 17:47:58 -0300

    cambie la funcion que muestra los productos destacados (agregue un form y un submit, para que los pueda pasar por metodo get)

--- TP/js/indexScript.js
+++ TP/js/indexScript.js
@@ -14,7 +14,9 @@ function mostrarProductosDestacados(){
     for (i=0;i<productosTotales.length;i++){
         if(productosTotales[i].destacado == true){
             miniatura = '<div class="col-2">';
-            miniatura += '<a href="'+productosTotales[i].seccion+'"><img src="'+productosTotales[i].img+'" class="img-thumbnail"></a></div>';
+            miniatura += '<form  method="get" action="'+productosTotales[i].seccion+'">'
+            miniatura += '<button type="submit"><img src="'+productosTotales[i].img+'" class="img-thumbnail"></button>';
+            miniatura += '<input type="hidden" name="idProducto" value="'+productosTotales[i].id+'"></div></form>'
             $('#thumbnailsContainer').append(miniatura);
         }
     }
