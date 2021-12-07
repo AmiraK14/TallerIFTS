e556052c3198068a166ec849a73f8cfc3b041898
AmiraK14 <amirakollrich@gmail.com>
2021-12-07 17:19:38 -0300

    modifique la funcion de validacion del form comentario (para que valide el mail cuando se ingresa un texto en el campo)

--- TP/js/musicaScript.js
+++ TP/js/musicaScript.js
@@ -59,58 +59,18 @@ function validarFormularioComentario(){
     comentario = $('#formComentarioComentario').val();
     if (mail == '') {
         llamarModalIncompleto('mail');
-    } else if (star1 == false && star2 == false && star3 == false && star4 == false && star5 == false){
-        llamarModalIncompleto('puntuacion');
-    } else if (comentario == '') {
-        llamarModalIncompleto('comentario');
     } else {
         if (validarEmail(mail) == false){
             alert('Debe ingresar una dirección de e-mail válida');
+        } else if (star1 == false && star2 == false && star3 == false && star4 == false && star5 == false){
+            llamarModalIncompleto('puntuacion');
+        } else if (comentario == '') {
+             llamarModalIncompleto('comentario');
         } else {
             llamarModalCompleto(mail);
             $('#formComentario').submit();
         }
     }
-}
-
-function llamarModalIncompleto(campo){
-    $('#modalsSection').empty();
-    miModal = '<div id="modalIncompleto" class="modal" tabindex="-1">'
-    miModal += '<div class="modal-dialog">'
-    miModal += '<div class="modal-content">'
-    miModal += '<div class="modal-header">'
-    miModal += '<h5 class="modal-title">Datos incompletos</h5>'
-    miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
-    miModal += '<div class="modal-body">'
-    miModal += '<p>Debe completar el campo '+campo+'</p></div>'
-    miModal += '<div class="modal-footer">'
-    miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
-    miModal += '</div></div></div></div>'
-    $('#modalsSection').append(miModal);
-    $('#modalIncompleto').show();
-    $('.botonCerrarModal').click(function(){
-        $('#modalIncompleto').hide();
-    })
-}
-
-function llamarModalCompleto(mail){
-    $('#modalsSection').empty();
-    miModal = '<div id="modalCompleto" class="modal" tabindex="-1">'
-    miModal += '<div class="modal-dialog">'
-    miModal += '<div class="modal-content">'
-    miModal += '<div class="modal-header">'
-    miModal += '<h5 class="modal-title">Envio exitoso</h5>'
-    miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
-    miModal += '<div class="modal-body">'
-    miModal += '<p>'+mail+' su comentario ha sido enviado</p></div>'
-    miModal += '<div class="modal-footer">'
-    miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
-    miModal += '</div></div></div></div>'
-    $('#modalsSection').append(miModal);
-    $('#modalCompleto').show();
-    $('.botonCerrarModal').click(function(){
-        $('#modalCompleto').hide();
-    })
 }
 
 function agregarComentario(){
