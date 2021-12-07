2e49e92257dab6384ecd6f04d7dd652c16537b2b
AmiraK14 <amirakollrich@gmail.com>
2021-12-07 17:49:03 -0300

    agregue el div modalsSection para agregar los modals que se agregan por funcion

--- TP/js/functions.js
+++ TP/js/functions.js
@@ -196,7 +196,7 @@ function llamarModalCompleto(mail){
     miModal += '<h5 class="modal-title">Envio exitoso</h5>'
     miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
     miModal += '<div class="modal-body">'
-    miModal += '<p>'+mail+' su comentario ha sido enviado</p></div>'
+    miModal += '<p><strong>'+mail+'</strong> su comentario ha sido enviado</p></div>'
     miModal += '<div class="modal-footer">'
     miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
     miModal += '</div></div></div></div>'
@@ -204,6 +204,26 @@ function llamarModalCompleto(mail){
     $('#modalCompleto').show();
     $('.botonCerrarModal').click(function(){
         $('#modalCompleto').hide();
+    })
+}
+
+function llamarModalMailInvalido(campo){
+    $('#modalsSection').empty();
+    miModal = '<div id="modalIncompleto" class="modal" tabindex="-1">'
+    miModal += '<div class="modal-dialog">'
+    miModal += '<div class="modal-content">'
+    miModal += '<div class="modal-header">'
+    miModal += '<h5 class="modal-title">E-mail invalido</h5>'
+    miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
+    miModal += '<div class="modal-body">'
+    miModal += '<p>Debe ingresar una dirección de correo electrónico válida</p></div>'
+    miModal += '<div class="modal-footer">'
+    miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
+    miModal += '</div></div></div></div>'
+    $('#modalsSection').append(miModal);
+    $('#modalIncompleto').show();
+    $('.botonCerrarModal').click(function(){
+        $('#modalIncompleto').hide();
     })
 }
 
