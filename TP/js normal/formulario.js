4dfdfc8364e58e27bc5d70b04ad42e7fa9b584e7
AmiraK14 <amirakollrich@gmail.com>
2021-12-14 18:53:12 -0300

    modal de envio muestra los datos ingresados

--- TP/js normal/formulario.js
+++ TP/js normal/formulario.js
@@ -19,7 +19,9 @@ function abrirModal() {
         comentario = $('#comentario').val();
         if (nombre == '' || apellido == '' || celular == '' || email == '' || comentario == '') {
             $('#modalUno').show();
-        } else { $('#modalDos').show(); }
+        } else { 
+            llamarModalCompleto(nombre, apellido, email, celular);
+        }
     })
 
 }
@@ -30,4 +32,27 @@ function cerrarModal() {
 
     })
 
+}
+
+function llamarModalCompleto(){
+    $('#modalsSection').empty();
+    miModal = '<div id="modalCompleto" class="modal" tabindex="-1">'
+    miModal += '<div class="modal-dialog">'
+    miModal += '<div class="modal-content">'
+    miModal += '<div class="modal-header">'
+    miModal += '<h5 class="modal-title">Envio exitoso</h5>'
+    miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
+    miModal += '<div class="modal-body">'
+    miModal += '<p><strong>'+nombre+' '+apellido+'</strong></p>'
+    miModal += '<p>'+email+'</p>'
+    miModal += '<p>'+celular+'</p>'
+    miModal += '<p>su comentario <i>'+comentario+'</i> ha sido enviado</p></div>'
+    miModal += '<div class="modal-footer">'
+    miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
+    miModal += '</div></div></div></div>'
+    $('#modalsSection').append(miModal);
+    $('#modalCompleto').show();
+    $('.botonCerrarModal').click(function(){
+        $('#modalCompleto').hide();
+    })
 }
