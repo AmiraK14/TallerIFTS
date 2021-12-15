0d074aef74244a6a503f69fb1797d38e03da5cea
Gonzalo <gonzalobarlaro97@gmail.com>
2021-12-14 23:42:15 -0300

    validacion de comentario

--- TP/js normal/descripcion.js
+++ TP/js normal/descripcion.js
@@ -96,3 +96,24 @@ request.onload = function(){
                 }
             }
         }      
+      function validarFormularioComentario(){
+            star5 = $('#star5').prop('checked');
+            star4 = $('#star4').prop('checked');
+            star3 = $('#star3').prop('checked');
+            star2 = $('#star2').prop('checked');
+            star1 = $('#star1').prop('checked');
+            mail = $('#mail').val();
+            comentario = $('#comentario').val();
+            if (star1 == false && star2 == false && star3 == false && star4 == false && star5 == false){
+                llamarModalIncompleto('puntuacion');
+            } else if (mail == '') {
+                llamarModalIncompleto('e-mail');
+            } else if (validarEmail(mail)==false) {
+                llamarModalMailInvalido();
+            } else if (comentario == '') {
+                llamarModalIncompleto('comentario');
+            } else {
+                llamarModalCompleto(mail);
+                $('#formComentarioShows').submit();
+            }
+        }
