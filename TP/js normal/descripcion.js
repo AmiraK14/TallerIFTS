162763750d8160b09afb64552cc34e0a3bd830d1
Gonzalo <gonzalobarlaro97@gmail.com>
2021-12-14 22:09:14 -0300

    asda

--- TP/js normal/descripcion.js
+++ TP/js normal/descripcion.js
@@ -12,8 +12,7 @@ request.onload = function(){
     console.log(localStorage.getItem('idDescripcion'));
     agregarCards(merch);
     //agregarCardsXParametro(array);
-    llamarModalCompleto();
-    llamarModalCompletoDeCompra();
+    //llamarModalCompleto();
 }
 
     function agregarNavbar(){
@@ -61,7 +60,21 @@ request.onload = function(){
                 }
             }
         }*/
-        function llamarModalCompleto(){         
+        
+        function llamarModal(){   
+           /* miModal = '<div id="modalCompleto" class="modal" tabindex="-1">'
+            miModal += '<div class="modal-dialog">'
+            miModal += '<div class="modal-content">'
+            miModal += '<div class="modal-header">'
+            miModal += '<h5 class="modal-title">Envio exitoso</h5>'
+            miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
+            miModal += '<div class="modal-body">'
+            miModal += '<p> su comentario ha sido enviado</p></div>'
+            miModal += '<div class="modal-footer">'
+            miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
+            miModal += '</div></div></div></div>'
+            $('#modalsSection').append();*/
+            $('#modalsSection').empty();
             miModal = '<div id="modalCompleto" class="modal" tabindex="-1">'
             miModal += '<div class="modal-dialog">'
             miModal += '<div class="modal-content">'
@@ -69,14 +82,19 @@ request.onload = function(){
             miModal += '<h5 class="modal-title">Envio exitoso</h5>'
             miModal += '<button type="button" class="btn-close botonCerrarModal" data-bs-dismiss="modal" aria-label="Close"></button></div>'
             miModal += '<div class="modal-body">'
-            miModal += '<p>s su comentario ha sido enviado</p></div>'
+           // miModal += '<p><strong>'+mail+'</strong> su comentario ha sido enviado</p></div>'
             miModal += '<div class="modal-footer">'
             miModal += '<button type="button" class="btn btn-secondary botonCerrarModal" data-bs-dismiss="modal">Cerrar</button>'
             miModal += '</div></div></div></div>'
-            $('#modalsSection').append();
-          
+            console.log(miModal); 
+            $('#modalsSection').append(miModal);
+            $('#modalCompleto').show();
+            $('.botonCerrarModal').click(function(){
+                $('#modalCompleto').hide();
+            })
         }
         
+      
        /* function agregarCardsXParametro(){
             if (getParameterByName('idProducto') != ''){
                 for(i=0;i<merch.length;i++){
